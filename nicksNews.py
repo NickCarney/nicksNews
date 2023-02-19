95d9e9a9c73671fcdcbf666f48f87210788602f4
Nick Carney <npcarney@uvm.edu>
2023-02-19 13:24:36 -0500

    starting to add upcoming games

--- nicksNews.py
+++ nicksNews.py
@@ -20,36 +20,38 @@ data = requests.get(url)
 my_data = []
 
 html = BeautifulSoup(data.text, 'html.parser')
-articles = html.select('tr')
+#articles = html.select('tr')
+articles = html.select('a.expandableTeam')
 print(articles)
 
 rank = 0
 counter = 0
 print("Premier League Table on",dt_string)
 for article in articles:
+    print(article)
     rank+=1
-    #name = article.select('.teamName')[0].get_text()
-    #str = "a./clubs/1/"+name+"/overview"   
-    #my_data.append({rank: name})
-    row = article.findAll('td')
-    currPosition = row[1].find('span', {'class': 'value'})
-    prevPosition = row[1].find('span', {'class': 'resultHighlight'}).text.strip()
-    team = row[2].find('span', {'class': 'long'}).text
+    name = article.select('.teamName')[0].get_text()
+    str = "a./clubs/1/"+name+"/overview"   
+    my_data.append({rank: name})
+    # row = article.findAll('td')
+    # currPosition = row[1].find('span', {'class': 'value'})
+    # prevPosition = row[1].find('span', {'class': 'resultHighlight'}).text.strip()
+    # team = row[2].find('span', {'class': 'long'}).text
 
-    gamesPlayed = row[3].text
+    # gamesPlayed = row[3].text
 
-    gamesWon = row[4].text
-    gamesLoss = row[6].text
-    draws = row[5].text
-    goalsFor = row[7].text
-    goalsAg = row[8].text
-    goalDiff = row[9].text.strip()
-    points = row[10].text
-    formAbv = []
-    for form in row[11].find_all('li'):
-        formAbv.append(form.find('abbr').text)
+    # gamesWon = row[4].text
+    # gamesLoss = row[6].text
+    # draws = row[5].text
+    # goalsFor = row[7].text
+    # goalsAg = row[8].text
+    # goalDiff = row[9].text.strip()
+    # points = row[10].text
+    # formAbv = []
+    # for form in row[11].find_all('li'):
+    #     formAbv.append(form.find('abbr').text)
     
-    print(team,gamesPlayed,gamesWon,draws,gamesLoss,goalsFor,goalsAg,goalDiff,points,formAbv)
+    # print(team,gamesPlayed,gamesWon,draws,gamesLoss,goalsFor,goalsAg,goalDiff,points,formAbv)
 
 #pointsList = html.find_all('a', class_=None)
 # table = html.find('table')
@@ -65,3 +67,17 @@ for article in articles:
 #     break
         
 pprint(my_data[:20])
+
+
+def showUpcoming():
+    soup = BeautifulSoup(data.content,'html.parser')
+    ul = soup.find('ul')
+    li = ul.find_all('li')
+    for li in li:
+        anchors = li.find('a.v z x y t h')
+        print(anchors)
+        team1 = li.find('.team')
+        print(team1)
+    #print(li)
+
+showUpcoming()
