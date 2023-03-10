b1e493926385ae3d449863b5db5f1f71d98d4c09
Nick Carney <npcarney@uvm.edu>
2023-03-10 15:50:44 -0500

    new

--- bball.py
+++ bball.py
@@ -13,12 +13,12 @@ for table in soup.find_all('table'):
     rows = table.find_all('tr')
     for row in rows:
         if('data-stat' in row):
-            name = row.get('th')
-            print(name.text)
+            name = row.get('data-stat')
+            print(name)
         data = row.find_all('td',class_=True)
         for datum in data:   
             #if(datum['class']):
-            print(datum['class'][0],end=":")        
+            print(datum['class'][0],end=" : ")        
             print(datum.text)
     print()
 
