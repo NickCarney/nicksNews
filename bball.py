82c7fb46895d4c6b295081600e217c30564bdd8a
Nick Carney <npcarney@uvm.edu>
2023-03-10 14:58:04 -0500

    adding bball scraping

---
+++ bball.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+URL = f'https://www.basketball-reference.com/boxscores/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+table = soup.find('table')
+
+head = table.find('thead')
+for table in soup.find_all('table'):
+    rows = table.find_all('tr')
+    for row in rows:
+        name = row.get('data-stat')
+        print(name)
+        data = row.find_all('td')
+        for datum in data:
+            print(datum.text, end=',')
+    print()
+
+    
