8e1865df9f7f1f6f5f3ae5df4b3f1ae62ce48c8c
Nick Carney <npcarney@uvm.edu>
2023-03-11 13:17:58 -0500

    changes

--- bball.py
+++ bball.py
@@ -6,12 +6,11 @@ URL = f'https://www.basketball-reference.com/boxscores/'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-table = soup.find('table')
 
-head = table.find('thead')
 for table in soup.find_all('table'):
     rows = table.find_all('tr')
     for row in rows:
+        #print(row)
         if('data-stat' in row):
             name = row.get('data-stat')
             print(name)
